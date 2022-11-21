6b6789ec7c3582e5083f662a18715fedd0efd627
bivanalhar <bivan.alzacky@gmail.com>
2022-11-22 00:58:04 +0800

    use templates to simplify html code

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import web
 
 @web.route('/')
-@web.route('/index')
+@web.route('/home')
 def index():
-    return "Welcome to the PIU Score Submitter website"
+    user = {'username' : 'Bivan'}
+    posts = [
+        {'author' : 'Ronald', 'body' : 'Teehee lemme torture you all with my UCS'},
+        {'author' : 'Aubrey', 'body' : 'OMG please no more, Senpai'},
+        {'author' : 'EL', 'body' : 'walao cannot anymore lah, my body really hurts now'},
+        {'author' : 'Kelvin', 'body' : 'NICEEE finally some display of passion'},
+        {'author' : 'Sky', 'body' : 'okay sure.. do all you want!!'},
+        {'author' : 'Zhiquan', 'body' : 'Eeeehhh???'}
+    ]
+    return render_template(
+        "main.html", 
+        user = user,
+        posts = posts
+    )
