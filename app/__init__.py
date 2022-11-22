25d32382acd8ab1c7d159889ba1ba596d1e4ad35
bivanalhar <bivan.alzacky@gmail.com>
2022-11-22 15:02:42 +0800

    making sign-in forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 web = Flask(__name__)
+web.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import web
+from app.forms import LoginForm
 
 def base(user1, user2, msg, address):
     user = {'username' : user1}
@@ -18,19 +19,29 @@ def base(user1, user2, msg, address):
     )
 
 @web.route('/')
-def index2():
+def index():
+    user = {'username' : "PIUNoobs"}
+    return render_template("main.html", user = user)
+
+@web.route('/main')
+def index1():
     return base(
         user1 = "Ronald",
         user2 = "Bivan",
         msg = 'Aigoo why such nonsense UCS again leh..',
-        address = "main.html"
+        address = "main1.html"
     )
 
 @web.route('/home')
-def index():
+def index2():
     return base(
         user1 = "Bivan",
         user2 = "Ronald",
         msg = 'Teehee lemme torture you all with my UCS',
         address = "main2.html"
     )
+
+@web.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title = "Sign In", form = form)
