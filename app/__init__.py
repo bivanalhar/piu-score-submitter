0155b264fda3af8b257ca8efb091536f9f71c381
bivanalhar <bivan.alzacky@gmail.com>
2022-12-05 17:57:35 +0800

    complete error handling and add validation for username

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging, os
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+
 web = Flask(__name__)
 web.config.from_object(Config)
 
@@ -14,3 +18,31 @@ db = SQLAlchemy(web)
 migrate = Migrate(web, db)
 
 from app import routes, models, errors
+
+if not web.debug:
+    if web.config['MAIL_SERVER']:
+        auth = None
+        if web.config['MAIL_USERNAME'] or web.config['MAIL_PASSWORD']:
+            auth = (web.config['MAIL_USERNAME'], web.config['MAIL_PASSWORD'])
+        secure = None
+        if web.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(web.config['MAIL_SERVER'], web.config['MAIL_PORT']),
+            fromaddr='no-reply@' + web.config['MAIL_SERVER'],
+            toaddrs=web.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        web.logger.addHandler(mail_handler)
+    
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/piu_blog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    web.logger.addHandler(file_handler)
+
+    web.logger.setLevel(logging.INFO)
+    web.logger.info('PIU blog startup')
