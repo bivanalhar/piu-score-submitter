c6f066297d6de847ab4d80ec5917d7f34cf4d3e1
wfxronald <cool.starmon@gmail.com>
2022-12-16 13:10:24 +0800

    Auto-populate username field

--- app/forms.py
+++ app/forms.py
@@ -39,7 +39,6 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("please use the different email address")
 
 class SubmissionForm1(FlaskForm):
-    username = StringField('PIU Username', validators = [DataRequired()])
     event = SelectField('Select the Event', choices = choices, validators = [DataRequired()])
     chart = SelectField('Select the Stepchart', choices = [], validators = [DataRequired()])
     perfect = IntegerField('Perfect')
@@ -52,10 +51,6 @@ class SubmissionForm1(FlaskForm):
     def __init__(self, original_username, *args, **kwargs):
         super(SubmissionForm1, self).__init__(*args, **kwargs)
         self.original_username = original_username
-    
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            raise ValidationError("please input the username used during login")
     
     def validate_perfect(self, perfect):
         if perfect.data < 0:
--- app/routes.py
+++ app/routes.py
@@ -134,7 +134,7 @@ def score():
     form.chart.choices = [(chart.id, chart.chart) for chart in Chart.query.filter_by(event = "E1").all()]
     if form.validate_on_submit():
         score = Score(username = form.username.data, perfect = form.perfect.data, great = form.great.data,
-            event = form.event.data, good = form.good.data, bad = form.bad.data, miss = form.miss.data,
+            event = current_user.username, good = form.good.data, bad = form.bad.data, miss = form.miss.data,
             chart = charts[form.chart.data])
         score.set_totalScore(form.perfect.data, form.great.data, form.good.data, 
             form.bad.data, form.miss.data)
@@ -143,7 +143,7 @@ def score():
 
         flash("Congratulations, {}, for you have successfully uploaded your score".format(form.username.data))
         return redirect(url_for('user', username=current_user.username))
-    return render_template("score.html", title = "Score Submission", form = form)
+    return render_template("score.html", title = "Score Submission", form = form, username = current_user.username)
 
 @web.route('/signup', methods=['GET', 'POST'])
 def signup():
